fa7ecb90b64a1b14c12427ff01d0a9fdf30e5354
Bela <belahamid@gmail.com>
2021-01-20 12:03:39 +0000

    db is seeded and connected

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -18,9 +18,21 @@ const createRouter = function (collection) {
             });
 
     });
-
-
+    
     //SHOW ONE /api/bookings/:id GET
+
+    router.get('/:id', (req,res) => {
+        const id = req.params.id;
+        collection
+            .findOne({ _id: ObjectID(id) })
+            .then((doc) => res.json(doc))
+            .catch((err) => {
+                console.error(err);
+                res.status(500);
+                res.json({status: 500, error: err});
+            });
+    });
+
     //CREATE /api/bookings POST
     //UPDATE /api/bookings/:id PUT
     //DELETE /api/bookings/:id DELETE
