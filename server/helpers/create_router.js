2c781f47c62ddc9e03bf2c4d86c5ffb68bea4c0c
Bela <belahamid@gmail.com>
2021-01-20 12:40:19 +0000

    all routes work

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -50,28 +50,21 @@ const createRouter = function (collection) {
 
     //UPDATE /api/bookings/:id PUT
 
-    // router.put('/:id', (req, res) => {
-    //     const id = req.params.id;
-    //     const updatedData = req.body;
-    //     collection
-    //         .updateOne(
-    //             {
-    //                 _id: ObjectID(id)
-    //             },
-    //             {
-    //                 $set: updatedData
-    //             }
-    //         )
-    //         .then((result) => {
-    //             res.json(result)
-    //         })
-    //         .catch((err) => {
-    //             console.error(err);
-    //             res.status(500);
-    //             res.json({ status: 500, error: err });
-    //         });
-
-    // });
+    router.put('/:id', (req, res) => {
+        const id = req.params.id;
+        const updatedData = req.body;
+        collection
+            .updateOne(
+                { _id: ObjectID(id) },
+                { $set: updatedData }
+            )
+            .then((result) => res.json(result))
+            .catch((err) => {
+                console.error(err);
+                res.status(500);
+                res.json({status: 500, error: err});
+            });
+    });
 
     //DELETE /api/bookings/:id DELETE
 
