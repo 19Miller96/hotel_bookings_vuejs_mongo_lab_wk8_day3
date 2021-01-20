c8b95cbead4fef2022b0bb77e39c3137245c02f9
Bela <belahamid@gmail.com>
2021-01-20 11:54:51 +0000

    /api/bookings GET request complete

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -6,10 +6,27 @@ const createRouter = function (collection) {
     const router = express.Router();
 
     //SHOW ALL /api/bookings GET
+    router.get('/', (req, res)=>{
+        collection
+            .find()
+            .toArray()
+            .then((docs) => res.json(docs))
+            .catch((err) => {
+                console.error(err);
+                res.status(500);
+                res.json({status: 500, error: err});
+            });
+
+    });
+
+
     //SHOW ONE /api/bookings/:id GET
     //CREATE /api/bookings POST
     //UPDATE /api/bookings/:id PUT
     //DELETE /api/bookings/:id DELETE
+
+    return router;
 }; 
+
 
 module.exports = createRouter;
